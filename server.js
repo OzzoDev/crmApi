a8fce06dbe44a872ff0d353e97d2fdd03eb7c309
= <=>
2024-11-09 22:50:26 +0100

    Get and post method for retriving and updating users from client

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const app = express();
 const port = 3000; 
 
 const corsOptions = {
-    origin:"",
+    origin:"*",
     methods:"GET,HEAD,PUT,PATCH,POST,DELETE",
     preflightContinue:false,
     optionSuccessStatus:204
@@ -16,6 +16,27 @@ const corsOptions = {
 
 app.use(cors(corsOptions));
 app.use(bodyParser.json()); 
+
+let users = [];
+
+//send users array to client
+app.get("/users",(req,res)=>{
+    res.json(users);
+})
+
+//apped to users array from client
+app.post("/users",(req,res)=>{
+    const newUser = req.body.user;
+
+    if(newUser){
+        users.push(newUser);
+        return res.status(201).json({message:"User added successfully",users}); 
+    }else{
+        return res.status(400).json({message:"User is required"})
+    }
+
+})
+
 
 //check server health
 app.get("/health",(req,res)=>{
